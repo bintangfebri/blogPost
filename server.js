bc1c2d4febe58c7ef8b4262a36432617dfaa8bd3
bintangdef <mbintangfebri@gmail.com>
2026-06-14 14:14:09 +0700

    feat: initialize express server and ejs skeleton

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log("server successfully running on port 3000");
+});
